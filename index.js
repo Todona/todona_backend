6d8b7b8c8887a69303c4f6bf29f18a288ae874ee
radsadorn <sutthiratphutho@gmail.com>
2021-04-18 21:00:46 +0700

    Update cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var corsOptions = {
   origin: process.env.CORS_ORIGIN
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
