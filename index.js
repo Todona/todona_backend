f243c704a22e08dec06d09b9f927da854d3c2016
SUTTHIRAT PHUTHO <61104175+radsadorn@users.noreply.github.com>
2023-03-08 20:09:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
